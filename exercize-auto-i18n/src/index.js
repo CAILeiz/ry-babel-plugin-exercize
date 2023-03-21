104abf43fe89c1835df06912d5ae31ef1c7f4e86
ryanzhouFQJ <ryanzhouFQJ>
2023-03-21 13:12:37 +0800

    feat: 自动国际化 debugger 及主要流程注释

--- exercize-auto-i18n/src/index.js
+++ exercize-auto-i18n/src/index.js
@@ -1,22 +1,30 @@
-const { transformFromAstSync } = require('@babel/core');
-const  parser = require('@babel/parser');
-const autoI18nPlugin = require('./plugin/auto-i18n-plugin');
-const fs = require('fs');
-const path = require('path');
+const { transformFromAstSync } = require("@babel/core");
+const parser = require("@babel/parser");
+const autoI18nPlugin = require("./plugin/auto-i18n-plugin");
+const fs = require("fs");
+const path = require("path");
 
-const sourceCode = fs.readFileSync(path.join(__dirname, './sourceCode.js'), {
-    encoding: 'utf-8'
+// 1. 读取源码
+const sourceCode = fs.readFileSync(path.join(__dirname, "./sourceCode.js"), {
+  encoding: "utf-8",
 });
 
+// 2. 通过 parse.parse 将源码转成 ast
 const ast = parser.parse(sourceCode, {
-    sourceType: 'unambiguous',
-    plugins: ['jsx']
+  sourceType: "unambiguous",
+  plugins: ["jsx"],
 });
 
+// 3. 通过 @babel/core 中的 transform 将 ast 通过插件转换中间代码
 const { code } = transformFromAstSync(ast, sourceCode, {
-    plugins: [[autoI18nPlugin, {
-        outputDir: path.resolve(__dirname, './output')
-    }]]
+  plugins: [
+    [
+      autoI18nPlugin,
+      {
+        outputDir: path.resolve(__dirname, "./output"), // 4. 配置输出目录
+      },
+    ],
+  ],
 });
 
 console.log(code);
--- exercize-auto-i18n/src/plugin/auto-i18n-plugin.js
+++ exercize-auto-i18n/src/plugin/auto-i18n-plugin.js
@@ -1,128 +1,170 @@
-const { declare } = require('@babel/helper-plugin-utils');
-const fse = require('fs-extra');
-const path = require('path');
-const generate = require('@babel/generator').default;
+const { declare } = require("@babel/helper-plugin-utils");
+const fse = require("fs-extra");
+const path = require("path");
+const generate = require("@babel/generator").default;
 
 let intlIndex = 0;
 function nextIntlKey() {
-    ++intlIndex;
-    return `intl${intlIndex}`;
+  ++intlIndex;
+  return `intl${intlIndex}`;
 }
 
+// 1. declare 声明一个插件
 const autoTrackPlugin = declare((api, options, dirname) => {
-    api.assertVersion(7);
+  api.assertVersion(7);
+  // 2. 如果没有目录, 直接返回并提示输出目录为空
+  if (!options.outputDir) {
+    throw new Error("outputDir in empty");
+  }
 
-    if (!options.outputDir) {
-        throw new Error('outputDir in empty');
+  function getReplaceExpression(path, value, intlUid) {
+    // 处理字符串或者模板字符串为 ast 表达式
+    const expressionParams = path.isTemplateLiteral()
+      ? path.node.expressions.map((item) => generate(item).code)
+      : null;
+    let replaceExpression = api.template.ast(
+      `${intlUid}.t('${value}'${
+        expressionParams ? "," + expressionParams.join(",") : ""
+      })`
+    ).expression;
+    if (
+      path.findParent((p) => p.isJSXAttribute()) &&
+      !path.findParent((p) => p.isJSXExpressionContainer())
+    ) {
+      replaceExpression = api.types.JSXExpressionContainer(replaceExpression);
     }
+    return replaceExpression;
+  }
 
-    function getReplaceExpression(path, value, intlUid) {
-        const expressionParams = path.isTemplateLiteral() ? path.node.expressions.map(item => generate(item).code) : null
-        let replaceExpression = api.template.ast(`${intlUid}.t('${value}'${expressionParams ? ',' + expressionParams.join(',') : ''})`).expression;
-        if (path.findParent(p => p.isJSXAttribute()) && !path.findParent(p=> p.isJSXExpressionContainer())) {
-            replaceExpression = api.types.JSXExpressionContainer(replaceExpression);
-        }
-        return replaceExpression;
-    }
+  function save(file, key, value) {
+    // 读取固定值保存在 intl 配置对象中, 具体保存在 state.file['_map'].allText 中
+    const allText = file.get("allText");
+    allText.push({
+      key,
+      value,
+    });
+    file.set("allText", allText);
+  }
 
-    function save(file, key, value) {
-        const allText = file.get('allText');
-        allText.push({
-            key, value
-        });
-        file.set('allText', allText);
-    }
+  return {
+    // 4. 给state中的file._map 设置 allText 为 []
+    pre(file) {
+      file.set("allText", []);
+    },
+    visitor: {
+      // 5. 定义 Program 钩子, 用来处理 intl 引入及 import 及 'i18n-disable' 的跳过字段标识(给node节点 skipTransform 打标)
+      Program: {
+        enter(path, state) {
+          let imported;
+          path.traverse({
+            ImportDeclaration(p) {
+              const source = p.node.source.value; // 此时值为 intl2, 所以 imported = undefined
+              if (source === "intl") {
+                imported = true;
+              }
+            },
+          });
+          if (!imported) {
+            const uid = path.scope.generateUid("intl");
+            const importAst = api.template.ast(`import ${uid} from 'intl'`);
+            path.node.body.unshift(importAst);
+            state.intlUid = uid;
+          }
 
-    return {
-        pre(file) {
-            file.set('allText', []);
-        },
-        visitor: {
-            Program: {
-                enter(path, state) {
-                    let imported;
-                    path.traverse({
-                        ImportDeclaration(p) {
-                            const source = p.node.source.value;
-                            if(source === 'intl') {
-                                imported = true;
-                            }
-                        }
-                    });
-                    if (!imported) {
-                        const uid = path.scope.generateUid('intl');
-                        const importAst = api.template.ast(`import ${uid} from 'intl'`);
-                        path.node.body.unshift(importAst);
-                        state.intlUid = uid;
+          path.traverse({
+            "StringLiteral|TemplateLiteral"(path) {
+              if (path.node.leadingComments) {
+                path.node.leadingComments = path.node.leadingComments.filter(
+                  (comment, index) => {
+                    if (comment.value.includes("i18n-disable")) {
+                      path.node.skipTransform = true;
+                      return false;
                     }
-
-                    path.traverse({
-                        'StringLiteral|TemplateLiteral'(path) {
-                            if(path.node.leadingComments) {
-                                path.node.leadingComments = path.node.leadingComments.filter((comment, index) => {
-                                    if (comment.value.includes('i18n-disable')) {
-                                        path.node.skipTransform = true;
-                                        return false;
-                                    }
-                                    return true;
-                                })
-                            }
-                            if(path.findParent(p => p.isImportDeclaration())) {
-                                path.node.skipTransform = true;
-                            }
-                        }
-                    });
-                }
+                    return true;
+                  }
+                );
+              }
+              if (path.findParent((p) => p.isImportDeclaration())) {
+                path.node.skipTransform = true;
+              }
             },
-            StringLiteral(path, state) {
-                if (path.node.skipTransform) {
-                    return;
-                }
-                let key = nextIntlKey();
-                save(state.file, key, path.node.value);
-
-                const replaceExpression = getReplaceExpression(path, key, state.intlUid);
-                path.replaceWith(replaceExpression);
-                path.skip();
-            },
-            TemplateLiteral(path, state) {
-                if (path.node.skipTransform) {
-                    return;
-                }
-                const value = path.get('quasis').map(item => item.node.value.raw).join('{placeholder}');
-                if(value) {
-                    let key = nextIntlKey();
-                    save(state.file, key, value);
-
-                    const replaceExpression = getReplaceExpression(path, key, state.intlUid);
-                    path.replaceWith(replaceExpression);
-                    path.skip();
-                }
-                // path.get('quasis').forEach(templateElementPath => {
-                //     const value = templateElementPath.node.value.raw;
-                //     if(value) {
-                //         let key = nextIntlKey();
-                //         save(state.file, key, value);
-
-                //         const replaceExpression = getReplaceExpression(templateElementPath, key, state.intlUid);
-                //         templateElementPath.replaceWith(replaceExpression);
-                //     }
-                // });
-                // path.skip();
-            },
+          });
         },
-        post(file) {
-            const allText = file.get('allText');
-            const intlData = allText.reduce((obj, item) => {
-                obj[item.key] = item.value;
-                return obj;
-            }, {});
-
-            const content = `const resource = ${JSON.stringify(intlData, null, 4)};\nexport default resource;`;
-            fse.ensureDirSync(options.outputDir);
-            fse.writeFileSync(path.join(options.outputDir, 'zh_CN.js'), content);
-            fse.writeFileSync(path.join(options.outputDir, 'en_US.js'), content);
+      },
+      // 6. 处理字符串
+      StringLiteral(path, state) {
+        if (path.node.skipTransform) {
+          return;
         }
-    }
+        // 获取唯一key, 如 'intl1'
+        let key = nextIntlKey();
+        // 将 key 跟 path.node.value 保存在 state.file['_map'].allText 中
+        save(state.file, key, path.node.value);
+
+        const replaceExpression = getReplaceExpression(
+          path,
+          key,
+          state.intlUid
+        );
+        // 替换节点并跳过
+        path.replaceWith(replaceExpression);
+        path.skip();
+      },
+      // 6. 处理模板
+      TemplateLiteral(path, state) {
+        if (path.node.skipTransform) {
+          return;
+        }
+        // 获取固定值, 中间用 {placeholder} 隔开
+        const value = path
+          .get("quasis")
+          .map((item) => item.node.value.raw)
+          .join("{placeholder}");
+
+        if (value) {
+          // 将 key 跟 path.node.value 保存在 state.file['_map'].allText 中
+          let key = nextIntlKey();
+          save(state.file, key, value);
+
+          const replaceExpression = getReplaceExpression(
+            path,
+            key,
+            state.intlUid
+          );
+          // 替换节点并跳过
+          path.replaceWith(replaceExpression);
+          path.skip();
+        }
+        // path.get('quasis').forEach(templateElementPath => {
+        //     const value = templateElementPath.node.value.raw;
+        //     if(value) {
+        //         let key = nextIntlKey();
+        //         save(state.file, key, value);
+
+        //         const replaceExpression = getReplaceExpression(templateElementPath, key, state.intlUid);
+        //         templateElementPath.replaceWith(replaceExpression);
+        //     }
+        // });
+        // path.skip();
+      },
+    },
+    // 7. 遍历 allText 用 fse 模块打印 输出 intl 配置对象
+    post(file) {
+      const allText = file.get("allText");
+      const intlData = allText.reduce((obj, item) => {
+        obj[item.key] = item.value;
+        return obj;
+      }, {});
+
+      const content = `const resource = ${JSON.stringify(
+        intlData,
+        null,
+        4
+      )};\nexport default resource;`;
+      fse.ensureDirSync(options.outputDir);
+      fse.writeFileSync(path.join(options.outputDir, "zh_CN.js"), content);
+      fse.writeFileSync(path.join(options.outputDir, "en_US.js"), content);
+    },
+  };
 });
 module.exports = autoTrackPlugin;
